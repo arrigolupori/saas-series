dcbff525c0314c5761c3bbbda3fc05a90c22c887
Arrigo Lupori <arrigo.lupori@gmail.com>
2021-12-17 21:59:16 +0100

    Navigation bar, sidebar, and work area

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#e52329',
+				secondary: '#f6f7f8',
+			},
+			height: {
+				v93: '93vh',
+			},
+		},
+	},
+	plugins: [],
+}
